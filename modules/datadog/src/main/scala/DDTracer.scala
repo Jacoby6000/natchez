a7168b5f9f27a281d9c9ebf96820d6f09ab3f820
Jacob Barber <Jacob.Barber@disneystreaming.com>
2020-10-01 12:32:18 -0500

    create unsafe entrypoint for datadog

--- modules/datadog/src/main/scala/DDTracer.scala
+++ modules/datadog/src/main/scala/DDTracer.scala
@@ -15,38 +15,40 @@ import scala.jdk.CollectionConverters._
 
 object DDTracer {
 
+  def unsafeEntryPoint[F[_]: Sync](tracer: NativeDDTracer) = {
+    new EntryPoint[F] {
+      override def root(name: String): Resource[F, Span[F]] =
+        Resource.make(
+          Sync[F].delay(tracer.buildSpan(name).start()))(
+          s => Sync[F].delay(s.finish()))
+        .map(DDSpan(tracer, _))
+
+      override def continue(name: String, kernel: Kernel): Resource[F, Span[F]] =
+        Resource.make(
+          Sync[F].delay {
+            val spanContext = tracer.extract(
+              Format.Builtin.HTTP_HEADERS,
+              new TextMapAdapter(kernel.toHeaders.asJava)
+            )
+            tracer.buildSpan(name).asChildOf(spanContext).start()
+          }
+        )(s => Sync[F].delay(s.finish())).map(DDSpan(tracer, _))
+
+      override def continueOrElseRoot(name: String, kernel: Kernel): Resource[F, Span[F]] =
+        continue(name, kernel) flatMap {
+          case null => root(name) // hurr, means headers are incomplete or invalid
+          case span => span.pure[Resource[F, *]]
+        }
+    }
+  }
+
+
   def entryPoint[F[_]: Sync](
     buildFunc: DDTracerBuilder => F[NativeDDTracer]
-  ): Resource[F, EntryPoint[F]] = {
+  ): Resource[F, EntryPoint[F]] =
     Resource.make(
       Sync[F].delay(NativeDDTracer.builder()).flatMap(buildFunc))(
-      s => Sync[F].delay(s.close()).void)
-      .map { tracer =>
-        new EntryPoint[F] {
-          override def root(name: String): Resource[F, Span[F]] =
-            Resource.make(
-              Sync[F].delay(tracer.buildSpan(name).start()))(
-              s => Sync[F].delay(s.finish()))
-            .map(DDSpan(tracer, _))
-
-          override def continue(name: String, kernel: Kernel): Resource[F, Span[F]] =
-            Resource.make(
-              Sync[F].delay {
-                val spanContext = tracer.extract(
-                  Format.Builtin.HTTP_HEADERS,
-                  new TextMapAdapter(kernel.toHeaders.asJava)
-                )
-                tracer.buildSpan(name).asChildOf(spanContext).start()
-              }
-            )(s => Sync[F].delay(s.finish())).map(DDSpan(tracer, _))
-
-          override def continueOrElseRoot(name: String, kernel: Kernel): Resource[F, Span[F]] =
-            continue(name, kernel) flatMap {
-              case null => root(name) // hurr, means headers are incomplete or invalid
-              case span => span.pure[Resource[F, *]]
-            }
-        }
-      }
-  }
+      s => Sync[F].delay(s.close()).void
+    ).map(unsafeEntryPoint(_))
 
 }
